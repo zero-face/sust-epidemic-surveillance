dfa6f628b8a656da83e3c5e7dbfa945ac8a8e0b6
zhuyunfei20010415 <2690534598@qq.com>
2021-10-11 00:10:23 +0800

    后台登录模块

--- src/main/java/com/example/epidemicsurveillance/mapper/RoleMapper.java
+++ src/main/java/com/example/epidemicsurveillance/mapper/RoleMapper.java
@@ -4,6 +4,8 @@ import com.example.epidemicsurveillance.entity.Role;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 /**
  * <p>
  * 角色表 Mapper 接口
@@ -15,4 +17,5 @@ import org.springframework.stereotype.Component;
 @Component
 public interface RoleMapper extends BaseMapper<Role> {
 
+    List<Role> getRoles(Integer adminId);
 }
